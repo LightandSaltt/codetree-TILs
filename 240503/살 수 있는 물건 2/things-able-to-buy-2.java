0aeb190c53a229a033b3c060e5bf7da0cdfea7af
Park Seung Min <99009678+LightandSaltt@users.noreply.github.com>
2024-05-03 17:00:22 +0900

    Title: 살 수 있는 물건 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240503/살 수 있는 물건 2/things-able-to-buy-2.java
+++ 240503/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -9,6 +9,8 @@ public class Main {
             System.out.println("book");
         } else if (n >= 1000) {
             System.out.println("mask");
+        } else if (n >= 500) {
+            System.out.println("pen");
         } else {
             System.out.println("no");
         }
