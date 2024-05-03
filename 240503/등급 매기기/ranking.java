44f96536455099beba4ab9509e611923a0c771eb
Park Seung Min <99009678+LightandSaltt@users.noreply.github.com>
2024-05-03 16:39:23 +0900

    Title: 등급 매기기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240503/등급 매기기/ranking.java
+++ 240503/등급 매기기/ranking.java
@@ -11,9 +11,9 @@ public class Main {
             System.out.println("B");
         } else if (score >= 70) {
             System.out.println("C");
-        } else if (score > 60) {
+        } else if (score >= 60) {
             System.out.println("D");
-        } else if (score <= 60) {
+        } else if (score < 60) {
             System.out.println("F");
         }
     }
