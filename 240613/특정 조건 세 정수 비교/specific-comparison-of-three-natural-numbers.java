ac07ff491ca1435030e8b37fbf7c6a8f2e792f5f
Park Seung Min <99009678+LightandSaltt@users.noreply.github.com>
2024-06-13 16:51:40 +0900

    Title: 특정 조건 세 정수 비교, Time: 153ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240613/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int min;
+
+        if (a > b && c > b) {
+            min = b;
+        } else if (b > a && c > a) {
+            min = a;
+        } else {
+            min = c;
+        }
+
+        if (a == c && a == b) {
+            System.out.println("1 0");
+        } else if (a == min ){
+            System.out.println("1 0");
+        }
+        else {
+            System.out.println("0 0");
+        }
+    }
+}
