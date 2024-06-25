36a47e75afc1dd88bea0a9771a54fc1c49a43c96
Park Seung Min <99009678+LightandSaltt@users.noreply.github.com>
2024-06-25 22:21:53 +0900

    Title: 윤년인가, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240625/윤년인가/is-leap-year.java
+++ 240625/윤년인가/is-leap-year.java
@@ -8,7 +8,7 @@ public class Main {
         if (y % 4 == 0) {
             System.out.println(true);
         } else {
-            if (y % 100 == 0 || y % 400 != 0) {
+            if (y % 100 == 0 && y % 400 != 0) {
                 System.out.println(false);
             } else {
                 System.out.println(false);
