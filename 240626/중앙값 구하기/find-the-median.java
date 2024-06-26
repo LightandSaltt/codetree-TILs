e0e37ca04828960a452a7337a0e753238996a9b9
Park Seung Min <99009678+LightandSaltt@users.noreply.github.com>
2024-06-26 09:44:12 +0900

    Title: 중앙값 구하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240626/중앙값 구하기/find-the-median.java
+++ 240626/중앙값 구하기/find-the-median.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
                 // 변수 선언, 입력
         int a = sc.nextInt();
         int b = sc.nextInt();
