ded25c1d9a5d5a777f1171a8c87cf38e8aa7b840
Park Seung Min <99009678+LightandSaltt@users.noreply.github.com>
2024-07-03 13:25:44 +0900

    Title: 모두 3의 배수, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240703/모두 3의 배수/all-multiples-of-3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+
+        if (a % 3 == 0 && b % 3 == 0 & c % 3 == 0 && d % 3 == 0 && e % 3 == 0){
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
